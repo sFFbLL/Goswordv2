e25456638d8431fe53f539492e57b2d65e964465
fliggy <cglanfliggy@163.com>
2022-04-08 20:09:46 +0800

    fix model完善

--- global/model.go
+++ global/model.go
@@ -8,9 +8,9 @@ import (
 
 type GSD_MODEL struct {
 	ID        uint           `gorm:"primarykey;autoIncrement"` // 主键ID
-	CreatedAt time.Time      // 创建时间
-	UpdatedAt time.Time      // 更新时间
 	CreateBy  uint           //创建人
 	UpdateBy  uint           //更新人
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
+	CreatedAt time.Time      // 创建时间
+	UpdatedAt time.Time      // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index;" json:"-"` // 删除时间
 }
--- model/system/sys_authority.go
+++ model/system/sys_authority.go
@@ -5,8 +5,8 @@ import "project/global"
 type SysAuthority struct {
 	global.GSD_MODEL
 	AuthorityId   uint          `json:"authorityId" gorm:"not null;primary_key;comment:角色ID;size:90"` // 角色ID
-	Level         uint          `json:"level" gorm:"comment:角色等级0最大"`
-	AuthorityName string        `json:"authorityName" gorm:"comment:角色名"` // 角色名
-	DataScope     string        `json:"dataScope"`
+	Level         uint          `json:"level" gorm:"not null;comment:角色等级0最大"`
+	AuthorityName string        `json:"authorityName" gorm:"not null;comment:角色名"` // 角色名
+	DataScope     string        `json:"not null;dataScope"`
 	SysBaseMenus  []SysBaseMenu `json:"menus" gorm:"many2many:sys_authority_menus;"`
 }
