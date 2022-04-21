bc8005559f9b1870fc1c627d648913b2c4e60861
fliggy <cglanfliggy@163.com>
2022-04-21 09:35:13 +0800

    feat login return defaultrouter

--- service/system/sys_user.go
+++ service/system/sys_user.go
@@ -50,7 +50,7 @@ func (userService *UserService) Register(u system.SysUser) (err error, userInter
 func (userService *UserService) Login(u *system.SysUser) (err error, userInter *system.SysUser) {
 	var user system.SysUser
 	u.Password = utils.MD5V([]byte(u.Password))
-	err = global.GSD_DB.Where("username = ? AND password = ?", u.Username, u.Password).Preload("Dept").Preload("Authorities.Depts").First(&user).Error
+	err = global.GSD_DB.Where("username = ? AND password = ?", u.Username, u.Password).Preload("Dept").Preload("Authority").Preload("Authorities.Depts").First(&user).Error
 	return err, &user
 }
 
