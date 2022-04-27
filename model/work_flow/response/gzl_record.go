88fa0acd234fb1b2ce4f670c2fc6f9d02a1a06d0
侯若桐 <1328085918@qq.com>
2022-04-27 16:42:04 +0800

    修改导入导出接口

--- model/work_flow/response/gzl_record.go
+++
@@ -1,8 +0,0 @@
-package response
-
-type MyInitiated struct {
-	AppName      string   `json:"appName"`
-	CurrentState uint8    `json:"currentState"` //当前状态(进行中1默认、已完成2、已结束3)
-	CurrentNode  string   `json:"currentNode"`  //当前节点
-	Inspector    []string `json:"inspector"`
-}
